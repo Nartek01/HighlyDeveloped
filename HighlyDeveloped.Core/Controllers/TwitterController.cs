a851ee2f25e5ac2b6627fcc4ea8bfa35203b59c6
Master Nara Lundmalm <nartek01@gmail.com>
2024-06-19 15:05:39 +0200

    Added Twitter Controller

---
+++ HighlyDeveloped.Core/Controllers/TwitterController.cs
@@ -0,0 +1,17 @@
+﻿//TwitterController.cs
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace HighlyDeveloped.Core.Controllers
+{
+    public class TwitterController : SurfaceController
+    {
+        // Controller action
+        public ActionResult GetTweets(string twitterHandle)
+        {
+            string PARTIAL_VIEW_FOLDER = "~/Views/Partials";
+
+            return PartialView(PARTIAL_VIEW_FOLDER + "/Latest Tweets.cshtml", twitterHandle);
+        }
+    }
+}
