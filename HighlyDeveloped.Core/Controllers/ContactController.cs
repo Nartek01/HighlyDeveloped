c9a6f6da097431c727d5e914806286535290e9b5
Master Nara Lundmalm <nartek01@gmail.com>
2024-06-19 20:52:34 +0200

    Created ContactController

---
+++ HighlyDeveloped.Core/Controllers/ContactController.cs
@@ -0,0 +1,26 @@
+﻿using HighlyDeveloped.Core.ViewModel;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace HighlyDeveloped.Core.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        //Summary
+        // This is for all operations regarding the contact form.
+        //Summary
+        public ActionResult RenderContactForm()
+        {
+            var viewModel = new ContactFormViewModel();
+
+            return PartialView("~/Views/Partials/Contact Form.cshtml", viewModel);
+            // call $ @Html.Action("RenderContactForm", "Contact", new { params... });
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HandleContactForm(ContactFormViewModel viewModel)
+        {
+            return null;
+        }
+    }
+}
