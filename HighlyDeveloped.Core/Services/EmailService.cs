d8a84fdc056eda549176ade1d43bceb6c95a5f0e
Master Nara Lundmalm <nartek01@gmail.com>
2024-08-02 12:23:45 +0200

    Created an Email service

---
+++ HighlyDeveloped.Core/Services/EmailService.cs
@@ -0,0 +1,14 @@
+﻿using HighlyDeveloped.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighlyDeveloped.Core.Services
+{
+    public class EmailService : IEmailService
+    {
+
+    }
+}
