e5b451c39f70a2c129457ae60072cc3e48c58933
Master Nara Lundmalm <nartek01@gmail.com>
2024-08-02 13:00:28 +0200

    Copied the email form ContactControllers.cs

--- HighlyDeveloped.Core/Services/EmailService.cs
+++ HighlyDeveloped.Core/Services/EmailService.cs
@@ -16,6 +16,47 @@ namespace HighlyDeveloped.Core.Services
         {
             _umbraco = umbraco;
         }
+        public void SendContactNotificationToAdmin(ContactFormViewModel viewModel)
+        {
+            // Read the email from and to addresses
+            // Get the site settings
+
+            var siteSettings = _umbraco.ContentAtRoot().DescendantsOrSelfOfType("siteSettings").FirstOrDefault();
+            if (siteSettings == null)
+            {
+                throw new Exception("There are no site settings");
+            }
+
+            var fromAddress = siteSettings.Value<string>("emailSettingsFromAddress");
+            var toAddresses = siteSettings.Value<string>("emailSettingsAdminAccounts");
+
+            if (string.IsNullOrEmpty(fromAddress))
+            {
+                throw new Exception("There needs to be a from address in the site settings");
+            }
+
+            if (string.IsNullOrEmpty(toAddresses))
+            {
+                throw new Exception("There needs to be a to address in the site settings");
+            }
+
+            // Construct the email
+            var emailSubject = "There has been  a contact form submitted";
+            var emailBody = $"A new contact form has been received from {viewModel.Name}";
+            var smtpMessage = new MailMessage();
+            smtpMessage.Subject = emailSubject;
+            smtpMessage.Body = emailBody;
+            smtpMessage.From = new MailAddress(fromAddress);
+
+            var toList = toAddresses.Split(',');
+            foreach (var item in toList)
+            {
+                if (!string.IsNullOrEmpty(item))
+                {
+                    smtpMessage.To.Add(item);
+                }
+            }
+            // send via email services
             try
             {
                 using (var smtp = new SmtpClient())
