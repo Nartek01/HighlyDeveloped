f17761160a4ace40781ba8e080e35923014c0bde
Master Nara Lundmalm <nartek01@gmail.com>
2024-08-02 12:23:57 +0200

    Update IEmailService.cs

--- HighlyDeveloped.Core/Interfaces/IEmailService.cs
+++ HighlyDeveloped.Core/Interfaces/IEmailService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HighlyDeveloped.Core.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace HighlyDeveloped.Core.Interfaces
 {
+    /// <summary>
+    /// Handles all email operations
+    /// </summary>
     public interface IEmailService
     {
+        void SendContactNotificationToAdmin(ContactFormViewModel viewModel);
     }
 }
