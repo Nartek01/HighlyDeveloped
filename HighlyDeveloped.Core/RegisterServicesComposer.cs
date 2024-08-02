13ef2ce0b1527d8a63577bb100c7d3a89a1ab6db
Master Nara Lundmalm <nartek01@gmail.com>
2024-08-02 12:58:40 +0200

    Create RegisterServicesComposer.cs

---
+++ HighlyDeveloped.Core/RegisterServicesComposer.cs
@@ -0,0 +1,21 @@
+﻿using HighlyDeveloped.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Security;
+
+namespace HighlyDeveloped.Core
+{
+    public class RegisterServicesComposer : IUserComposer
+    {
+        // Create a public "compose" method
+        public void Compose(Composition composition)
+        {
+            composition.Register<IEmailService, EmailService>(Lifetime.Request);
+        }
+    }
+}
